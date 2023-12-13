a0bca54969927d07c8bcefc3dc02494feafac59f
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-12-13 14:56:52 +0100

    first commit

---
+++ src/components/TheHeader.jsx
@@ -0,0 +1,21 @@
+import style from "../css/modules/TheHeader.module.css";
+import { Link } from "react-router-dom";
+export default function TheHeader() {
+  return (
+    <header className={style.header}>
+      <h1>Il mio Blog</h1>
+      <Link to="/">
+        {" "}
+        <p>Home Page</p>{" "}
+      </Link>
+      <Link to="/form">
+        {" "}
+        <p>Add Post</p>
+      </Link>
+      <Link to="/login">
+        {" "}
+        <p>Login</p>
+      </Link>
+    </header>
+  );
+}
